05956fbd694db3b55bf4414e773a0aceea1a2e61
Jacques Bernier <jdesjardinsbernier@gmail.com>
2022-02-08 20:01:53 -0800

    example test

--- retry/example_test.go
+++ retry/example_test.go
@@ -1,6 +1,40 @@
 package retry
 
+import (
+	"context"
+	"errors"
+	"fmt"
+)
+
 func ExampleFoo() {
-	Foo()
-	// Output: bar
+	retrier, _ := NewRetry()
+
+	ctx := context.TODO()
+
+	// We don't have retry tokens to start
+	callCount := 0
+	_ = retrier.Execute(ctx, func(ctx context.Context) error {
+		callCount += 1
+		return errors.New("oh no!")
+	})
+	fmt.Println(callCount)
+
+	// Accumulate retry tokens by successfuly completing some operations
+	for i := 0; i < 100; i++ {
+		_ = retrier.Execute(ctx, func(ctx context.Context) error {
+			// do something that does not error
+			return nil
+		})
+	}
+
+	// use your retry tokens
+	callCount = 0
+	_ = retrier.Execute(ctx, func(ctx context.Context) error {
+		callCount += 1
+		return errors.New("oh no!")
+	})
+
+	fmt.Println(callCount)
+	// Output: 1
+	// 2
 }
--- retry/retry.go
+++ retry/retry.go
@@ -1,7 +1,52 @@
 package retry
 
-import "fmt"
+import (
+	"context"
+	"sync/atomic"
+)
 
-func Foo() {
-	fmt.Println("bar")
+func NewRetry() (*retrier, error) {
+	return &retrier{
+		every:    99,
+		counter:  0,
+		token:    0,
+		maxToken: 10,
+	}, nil
+}
+
+type retrier struct {
+	every uint64
+
+	counter  uint64
+	token    int64
+	maxToken int64
+}
+
+func (r *retrier) Execute(ctx context.Context, op func(context.Context) error) error {
+	err := op(ctx)
+	if err != nil {
+		newBalance := atomic.AddInt64(&r.token, -1)
+		if newBalance >= 0 {
+			return r.Execute(ctx, op)
+		}
+
+		// We can't go negative. So we add it back.
+		if newBalance == -1 {
+			atomic.AddInt64(&r.token, 1)
+		}
+		return err
+	}
+
+	// record success
+	newCount := atomic.AddUint64(&r.counter, 1)
+	if newCount%r.every == 0 {
+		// Add a token
+		newToken := atomic.AddInt64(&r.token, 1)
+
+		// Don't let the token balance go above the limit
+		if newToken > r.maxToken {
+			atomic.AddInt64(&r.token, -1)
+		}
+	}
+	return nil
 }
